546c06f1288ea673346ac3d720f67cd01e0abb01
oriyomi1976 <sanniyomi1@gmail.com>
2025-10-17 20:28:03 -0400

    variable modified

--- prod/variables.tf
+++ prod/variables.tf
@@ -85,13 +85,13 @@ variable "key_name" {
 variable "domain_name" {
   description = "Primary domain name for certificate issuance"
   type        = string
-  default     = "*.dominionsystem.org"
+  default     = "*.escalantetech.click"
 }
 
 variable "san_domains" {
   description = "SANs (Subject Alternative Names) for SSL certificate"
   type        = list(string)
-  default     = ["*.dominionsystem.org"]
+  default     = ["*.escalantetech.click"]
 }
 
 variable "route53_zone_id" {
