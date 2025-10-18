f4b2416cd804cd58403513d11efa5aec1ff787a3
oriyomi1976 <sanniyomi1@gmail.com>
2025-10-17 23:18:18 -0400

    new acm update

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_acm_certificate" "fusisoft_cert" {
+resource "aws_acm_certificate" "escalantetech_cert" {
   domain_name       = var.domain_name
   subject_alternative_names = var.san_domains
   validation_method = "DNS"
@@ -34,6 +34,6 @@ resource "aws_route53_record" "cert_validation" {
 
 
 resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = aws_acm_certificate.fusisoft_cert.arn
+  certificate_arn         = aws_acm_certificate.escalantetech_cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
