60e865d2d6f1fcacaba79be86e1a62192936108d
oriyomi1976 <sanniyomi1@gmail.com>
2025-10-17 23:56:18 -0400

    updated line 17

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -14,7 +14,7 @@ resource "aws_acm_certificate" "escalantetech_cert" {
 
 resource "aws_route53_record" "cert_validation" {
   for_each = {
-    for dvo in aws_acm_certificate.escalantetech_cert :
+    for dvo in aws_acm_certificate.escalantetech_cert.domain_validation_options :
     dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
